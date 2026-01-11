405b054e87e1a89b904aedb53ac7debc9c04e79b
FichesteR <viplanellis2@gmail.com>
2026-01-11 02:33:50 -0300

    chore: adição de geração de evidências por screenshots e videos ao projeto

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,16 @@
-module.exports = {
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://www.saucedemo.com',
+    screenshotOnRunFailure: true,
+    video: true,
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      on('after:screenshot', (details) => {
+        console.log('📸 Screenshot gerado:', details.path);
+      });
+
+      return config;
     },
   },
-};
+});
